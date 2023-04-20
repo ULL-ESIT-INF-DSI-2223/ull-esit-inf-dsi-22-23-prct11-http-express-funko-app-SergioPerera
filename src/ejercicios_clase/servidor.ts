8aa35ff2a2f3449cebe0a57586f9014e13e5acb7
SergioPerera <alu0101394503@ull.edu.es>
2023-04-20 09:41:36 +0000

    pasa todo los test

--- src/ejercicios_clase/servidor.ts
+++ src/ejercicios_clase/servidor.ts
@@ -1,11 +1,48 @@
 import express from 'express';
+import { exec } from 'child_process';
 
+/// Generamos el servidor express
 const app = express();
+/// Lo ponemos a escuchar en el puerto 3000
+const PORT = 3000;
 
-app.get('', (_, res) => {
-  res.send('Hello World!');
+/// Usamos json ya que no vamos a usar archivos estáticos
+app.use(express.json());
+
+/// Ponemos el punto de acceso json que devuelve un json
+app.get('/execmd', (req, res) => {
+  const cmd = req.query.cmd;
+  /// Hacemos que el parámetro args sea opcional
+  const args = req.query.args || '';
+
+
+  /// Ponemos como obligatorio el parámetro cmd, como el args es opcional no hace falta que lo comprobemos
+  if (!cmd) {
+    return res.status(400).json({ error: 'Missing cmd parameter' });
+  }
+
+  /// Ejecutamos el comando que nos ha llegado
+  exec(`${cmd} ${args}`, (error, stdout, stderr) => {
+    /// Si el comando no existe 
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+    /// Si el comando emite algún error
+    if (stderr) {
+      return res.status(500).json({ error: stderr });
+    }
+
+    /// Devolvemos el resultado del comando
+    return res.json({ output: stdout });
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+/// En caso de que no accedamos correctamente al punto de acceso
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not found' });
+});
+
+/// Ponemos el servidor a escuchar
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
---
+++ tests/servidor.spec.ts
@@ -0,0 +1,54 @@
+
+import 'mocha';
+import { expect } from 'chai';
+import request from 'request';
+import express from 'express';
+
+const app = express();
+
+describe('Test server app', () => {
+  describe('GET /execmd', () => {
+
+    it('should return output of command', (done) => {
+      const cmd = 'echo';
+      const args = 'hello world';
+      const expectedOutput = 'hello world\n';
+
+      request.get(`http://localhost:3000/execmd?cmd=${cmd}&args=${args}`, (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(JSON.parse(body)).to.deep.equal({ output: expectedOutput });
+        done();
+      });
+    });
+
+    it('should return an error if missing cmd parameter', (done) => {
+      request.get('http://localhost:3000/execmd', (err, res, body) => {
+        expect(res.statusCode).to.equal(400);
+        expect(JSON.parse(body)).to.deep.equal({ error: 'Missing cmd parameter' });
+        done();
+      });
+    });
+
+    it('should return an error if command does not exist', (done) => {
+      const cmd = 'nonexistentcommand';
+
+      request.get(`http://localhost:3000/execmd?cmd=${cmd}`, (err, res, body) => {
+        expect(res.statusCode).to.equal(500);
+        expect(JSON.parse(body)).to.deep.equal({ error: `Command failed: ${cmd} \n/bin/sh: 1: ${cmd}: not found\n` });
+        done();
+      });
+    });
+
+    it('should return an error if command emits an error', (done) => {
+      const cmd = 'ls';
+      const args = 'nonexistentfile';
+    
+      request.get(`http://localhost:3000/execmd?cmd=${cmd}&args=${args}`, (err, res, body) => {
+        expect(res.statusCode).to.equal(500);
+        expect(JSON.parse(body)).to.have.property('error');
+        done();
+      });
+    });
+
+  });
+});
